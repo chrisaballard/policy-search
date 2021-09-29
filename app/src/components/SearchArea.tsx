c62c02491801c73e608f5e76ea51375f1b050018
Paula Hightower <eurolife@gmail.com>
2021-09-29 19:35:51 +0100

    WIP search input UI

--- app/src/components/SearchArea.tsx
+++ app/src/components/SearchArea.tsx
@@ -1,27 +1,60 @@
-import { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface SearchAreaProps {
-
+  onChange(e: React.FormEvent<HTMLInputElement>): void;
 }
-const SearchArea = (props: SearchAreaProps): JSX.Element => {
+const SearchArea = ({onChange}: SearchAreaProps): JSX.Element => {
   const [ searchOpen, setSearchOpen ] = useState(false);
   
-  const searchButton = useRef();
-  const searchInput = useRef();
+  const searchButton = useRef<HTMLButtonElement>(null);
+  const searchInput = useRef<HTMLInputElement>(null);
+  const searchLabel = useRef<HTMLLabelElement>(null);
 
-  const handleClick = (e): void => {
+  const handleClick = (e: React.FormEvent<HTMLInputElement> ): void => {
     e.preventDefault();
     setSearchOpen(!searchOpen);
   }
-
+  useEffect(() => {
+    if(searchInput.current && !searchOpen) {
+      searchInput.current.value = '';
+    }
+      setTimeout(() => {
+        if(searchInput.current && searchOpen) {
+          searchInput.current.focus();
+        }
+      }, 800);
+  }, [searchOpen])
   return (
-    <div className="pt-12 search-area flex flex-col justify-center items-center">
+    <div className="pt-16 search-area flex flex-col justify-center items-center">
       <h1 className="text-6xl font-bold text-black transform-uppercase">Policy Search</h1>
-      <form className="relative h-48 w-1/2 mx-auto ml-1/4">
-        <input ref={searchInput} type="text" className={`h-20 bg-white border-b-8 border-black text-3xl focus ${searchOpen ? 'square': ''}`} />
-        <button ref={searchButton} onClick={handleClick} className={`search-btn ${searchOpen ? 'close' : ''}`}></button>
+      <form className="absolute top-0 mt-56 h-48 w-1/2 mx-auto">
+        <label 
+          ref={searchLabel}
+          className={`block absolute top-0 left-0 right-0 text-gray-400 text-2xl text-center z-10 pointer-events-none transition duration-300 ${searchOpen ? 'opacity-0': 'opacity-100'}`}>
+            What are you looking for?
+        </label>
+        <div 
+          className={`search-input-wrapper transition-all duration-300 ${searchOpen ? 'expanded' : ''}`}>
+            <input 
+              ref={searchInput}
+              onChange={onChange}
+              className={`search-input h-full w-full text-xl text-gray-500 outline-none focus:outline-none`} 
+            />
+        </div>
+        
+        <button
+          onClick={handleClick} ref={searchButton}
+          className={`search-btn outline-none focus:outline-none flex items-center justify-end ${searchOpen ? 'collapsed' : ''}`}>
+          <img 
+            src="/images/close.svg" 
+            alt="Close icon"
+            className={`search-btn-close ${searchOpen ? 'opacity-100': 'opacity-0'}`} />
+            <img 
+              src="/images/search.svg" 
+              alt="Search icon" 
+              className={`search-btn-search ${searchOpen ? 'opacity-0': 'opacity-100'}`} />
+        </button>
       </form>
-      
     </div>
   )
 
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,19 +1,23 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { getPolicies } from '../lib/api';
+import { getPolicies } from '../api';
 import Link from 'next/link';
 import Layout from '../components/Layout'
 import SearchArea from '../components/SearchArea';
 import { Head } from 'next/document';
 
 export default function Home() {
-
+  const [ policies, setPolicies ] = useState([])
+  const handleChange = async () => {
+    const list = await getPolicies();
+    setPolicies(list);
+  }
   return (
     <Layout>
       {/* <Head>
         <title>Policy Search</title>
       </Head> */}
-      <SearchArea />
+      <SearchArea onChange={handleChange} />
     </Layout>
   )
 }
