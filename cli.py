1fd0effc709fe277da43249df0f566f2bd465495
chrisaballard <chris@climatepolicyradar.org>
2021-09-30 16:46:17 +0000

    Changes to the cli to add callbacks for dynamodb and the dataset - note: elastic search still needs to be refactored to work this way.

--- cli.py
+++ cli.py
@@ -15,17 +15,21 @@ from policy_search.pipeline.fetch import CSVDocumentSourceFetcher
 from policy_search.pipeline.dynamo import PolicyDynamoDBTable
 from policy_search.pipeline.elasticsearch import ElasticSearchIndex
 from policy_search.parser.pdf_parser import PDFParser
+from policy_search.pipeline.processor import DocumentProcessor
+from policy_search.pipeline.dataset import PolicyTextDataset
 from policy_search.search.results import format_policy_search_results, pretty_print_answers
 
 
 TXT_FILEAME_ATTRIBUTE = 'policy_txt_file'
 DOC_FILENAME_ATTRIBUTE = 'policy_content_file'
 
+with open('./config.yml', 'rt') as config_f:
+    config = yaml.load(config_f)
+
 @click.group()
 @click.pass_context
 def main(ctx):
     ctx.ensure_object(dict)
-    #ctx.obj['document_store'] = ElasticsearchDocumentStore(index='policy', name_field='policy_name')
 
 @main.command()
 @click.pass_context
@@ -39,47 +43,39 @@ def load(ctx, data_path: Path, csv_filename: Path, doc_filename_attribute: str):
     data_path = Path(data_path)
     csv_filename = data_path / csv_filename
 
-    with open('./config.yml', 'rt') as config_f:
-        config = yaml.load(config_f)
-
-    # Get initialised document store from click context
-    # document_store = ctx.obj['document_store']
-    # load_documents_from_csv(
-    #     csv_filename,
-    #     doc_path,
-    #     doc_filename_attribute,
-    #     document_store,
-    #     config['attributes']
-    # )
-
     dynamodb_host = os.environ.get('dynamodb_host', 'localhost')
     dynamodb_port = os.environ.get('dynamodb_port', '8000')
     dynamodb_url = f'http://{dynamodb_host}:{dynamodb_port}'
 
     cclw_attributes = config['sources']['cclw']['attributes']
-    doc_fetcher = CSVDocumentSourceFetcher(csv_filename, doc_filename_attribute, cclw_attributes)
+    doc_fetcher = CSVDocumentSourceFetcher(
+        csv_filename, 
+        doc_filename_attribute, 
+        cclw_attributes,
+        50
+    )
 
-    # Load policy documents into dynamodb
-    policy_table = PolicyDynamoDBTable(dynamodb_url, 'policyId')
-    policy_table.load(doc_fetcher)
+    # Initialise dynamodb table
+    dynamodb_table = PolicyDynamoDBTable(dynamodb_url, 'policyId')
 
-    # Load policy text into elastic search document store
-    elastic_host = os.environ.get('elasticsearch_cluster', 'localhost:9200')
+    # Initialise pdf document parser
     doc_parser = PDFParser(data_path, 'content', 'text', save_pdf_text=True)
-    es = ElasticSearchIndex(es_url=elastic_host)
-    es.load_documents(doc_fetcher, doc_parser)
 
+    # Initialise policy text dataset
+    dataset = PolicyTextDataset(Path('./data/policy_dataset.csv'))
 
-@main.command()
-@click.pass_context
-def query(ctx):
-    # Get initialised document store from click context
-    document_store = ctx.obj['document_store']
-    retriever = ElasticsearchRetriever(document_store=document_store)
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=False)
-    policy_search_pipeline = ExtractiveQAPipeline(reader, retriever)
-    search_prediction = policy_search_pipeline.run(query='what incentives are used to encourage ev takeup')
-    pretty_print_answers(format_policy_search_results(search_prediction))
+    # Initialise elastic search
+    elastic_host = os.environ.get('elasticsearch_cluster', 'localhost:9200')
+    #es = ElasticSearchIndex(es_url=elastic_host)
+
+    # Initialise document processor, add callback objects and process text
+    doc_processor = DocumentProcessor(doc_fetcher, doc_parser)
+    doc_processor.add_callback(dataset)
+    doc_processor.add_callback(dynamodb_table)
+    #doc_processor.add_callback(es)
+    doc_processor.process_text()
+    
+    # es.load_documents(doc_fetcher, doc_parser)
 
 
 if __name__ == '__main__':
