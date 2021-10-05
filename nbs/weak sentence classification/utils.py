fb9529cb8fd9b4fae6064cc057379b9ceb79825f
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-10-05 16:41:33 +0100

    Zero-shot notebook uses keywords concatenated with subsectors

--- nbs/weak sentence classification/utils.py
+++ nbs/weak sentence classification/utils.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 import yaml
 from transformers import pipeline
@@ -48,8 +49,15 @@ class Schema:
         return kwd_subsector_mapping
     
 class ZeroShotClassifier:
-    def __init__(self, schema: Schema, huggingface_pipeline_name: str = "typeform/distilbert-base-uncased-mnli", multi_label: bool = True):
+    def __init__(
+        self, 
+        schema: Schema, 
+        huggingface_pipeline_name: str = "typeform/distilbert-base-uncased-mnli", 
+        concat_keywords_with_subsectors: bool = False,
+        multi_label: bool = True,
+    ):
         self.schema = schema
+        self.concat_keywords_with_subsectors = concat_keywords_with_subsectors
         self.multi_label = multi_label
         self.clf = self._load_pipeline_from_huggingface(huggingface_pipeline_name)
         
@@ -58,9 +66,21 @@ class ZeroShotClassifier:
     def _load_pipeline_from_huggingface(self, name: str):
         return pipeline("zero-shot-classification", model=name)
     
+    def _keyword_subsector_concatenator(self, kwd: str):
+        """string modifier for _embed_keywords"""
+        
+        return f"{kwd} {self._keyword_subsector_mapping[kwd]}"
+    
+    def _keyword_from_concatenated_keyword_subsector(self, _str: str) -> str:
+        
+        return re.findall(r"([a-z\s-]+)\s[A-Z]", _str)[0]
+    
     @property
     def _class_labels(self):
-        return self.schema.all_keywords
+        if self.concat_keywords_with_subsectors:
+            return [self._keyword_subsector_concatenator(k) for k in self.schema.all_keywords]
+        else:
+            return self.schema.all_keywords
         
     def predict(self, text: str, threshold: float):
         pipeline_result = self.clf(text, self._class_labels, multi_label=self.multi_label)
@@ -69,9 +89,10 @@ class ZeroShotClassifier:
         for idx in range(len(pipeline_result['labels'])):
             if pipeline_result['scores'][idx] >= threshold:
                 _label = pipeline_result['labels'][idx]
+                _keyword = self._keyword_from_concatenated_keyword_subsector(_label) if self.concat_keywords_with_subsectors else _label
                 _score = pipeline_result['scores'][idx]
                 res.append(
-                    (_label, self._keyword_subsector_mapping[_label], pipeline_result['scores'][idx])
+                    (_keyword, self._keyword_subsector_mapping[_keyword], pipeline_result['scores'][idx])
                 )
 
         return res
