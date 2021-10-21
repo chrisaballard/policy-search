a84a557958c62517e8c7d980d87f7f8c7b2f7042
Paula Hightower <eurolife@gmail.com>
2021-10-21 17:14:22 +0100

    New text icon, new search results layout (WIP)

--- app/src/components/search/SearchResultItem.tsx
+++ app/src/components/search/SearchResultItem.tsx
@@ -4,6 +4,9 @@ import TextExtract from "../blocks/TextExtract";
 import { getCountryNameFromCode } from '../../helpers/geography';
 import { ResultByPage } from "../../model/resultByPage";
 import { ResultByDocument } from "../../model/resultByDocument";
+import { PDFIcon, TextIcon } from '../elements/images/SVG';
+import Link from 'next/link'
+import Circle from "../blocks/Circle";
 
 interface SearchResultItemProps {
   policy: ResultByDocument;
@@ -30,43 +33,44 @@ const SearchResultItem = ({policy, geographyList, texts}: SearchResultItemProps)
     getCountryName();
   }, [])
   return (
-    <div>
-      <div className="grid grid-cols-7 md:grid-cols-5 gap-x-4">
-        <div className="col-span-5 md:col-span-4 pr-6">
-          <h3 className="font-bold">{policy.policyName}</h3>
-          
+    <div className="font-bold grid grid-cols-8 md:grid-cols-6 gap-x-4 md:py-2">
+      <div className="col-span-5 sm:col-span-6 md:col-span-4 flex items-center">
+        <div className="mr-4">
+          <div style={{ width: '28px', height: '21px' }} className={`rounded border border-black flag-icon-background flag-icon-${policy.countryCode.toLowerCase()}`} />
         </div>
-        <div className="relative">
-            <div className="absolute top-0 left-0 flex flex-col items-center w-full">
-              <div className={`rounded border border-black flag-icon-background flag-icon-${policy.countryCode.toLowerCase()}`} />
-              <span className="text-xs text-center text-gray-500 mt-2">{country}</span>
-            </div>
+        <div className="leading-tight">
+          {policy.policyName}
+          <div className="md:hidden w-full mt-2 font-normal text-primary-dark-500">2021</div>
         </div>
+        
       </div>
-      <div className="text-sm mt-2 text-primary-dark-500">
-      {texts.length > 0 ?
-        <>
-          <button
-            onClick={toggleExtracts}
-            className="font-bold focus:outline-none text-primary-light hover:text-primary transition duration-300"
-          >
-            {texts.length} page match{`${texts.length > 1 ? 'es' : ''}`} 
-          </button> in this policy.
-        </>
-        : 
-          <span className="">{texts.length} page matches.</span> 
-        }
-      </div>
-
-      <div className={`${!showExtracts ? 'hidden' : ''}`}>
-        {texts.map((item, index) => (
-          <TextExtract
-            key={`${index}_${item.pageNumber}`}
-            texts={item.text}
-            page={item.pageNumber}
-            id={policy.policyId}
-          />
-        ))}
+      <div className="hidden md:block text-center font-normal text-primary-dark-500">2021</div>
+      <div className="col-span-3 sm:col-span-2 md:col-span-1">
+        <div className="flex justify-end">
+          <Link href={`/policy/${policy.policyId}?page=1`}>
+            <a href={``} className="flex flex-col justify-center items-center">
+              <span className="sr-only">Read text</span>
+              <Circle
+                title="Read Text"
+                bgClass="bg-secondary-3"
+                textClass="text-white"
+              >
+                <TextIcon height="16" width="16" />
+              </Circle>
+            </a>
+          </Link> 
+          <a href={`policy.url`} target="_blank" rel="noopener noreferrer" className="ml-4 flex flex-col justify-center items-center">
+            <span className="sr-only">View PDF</span>
+            <Circle
+              title="View PDF"
+              bgClass="bg-secondary-4"
+              textClass="text-white"
+            >
+              <PDFIcon height="16" width="16" />
+            </Circle>
+            
+          </a>
+        </div>
       </div>
     </div>
     
--- app/src/components/search/SearchResults.tsx
+++ app/src/components/search/SearchResults.tsx
@@ -38,34 +38,41 @@ const SearchResults = ({
   return (
     <section className="w-full">
         <div className="pt-8 md:pt-0 md:pl-4">
-          {searchTerms.length ?
-            <div className="mt-4">
-              {searchTerms.length && !processing ? renderMessage() : null}
-            </div>
-            : null
-          }
-          {policies.length ?
-            <>
-              <div className="font-bold md:text-lg grid grid-cols-7 md:grid-cols-5 gap-x-4 mt-8 border-primary border-t border-b py-2">
-                <div className="col-span-5 md:col-span-4">Policy</div>
-                <div className="text-center">Country</div>
-              </div>
-              <ul className="mt-4">
-                {policies.map((policy, index) => (
-                  <li 
-                    className="search-result border-b border-gray-300 border-dotted last:border-none py-6"
-                    key={`${index}-${policy.policyId}`}
-                    id={`${index}-${policy.policyId}`}
-                  >
-                    <SearchResultItem policy={policy} geographyList={geographyList} texts={policy.resultsByPage} />
-                  </li>
-              ))}
-              </ul>
-            </>
-          :
-          null}
+          {/* <h2 className="text-2xl font-bold leading-tight">Policies</h2> */}
+        {searchTerms.length ?
+          <div className="mt-4">
+            {searchTerms.length && !processing ? renderMessage() : null}
+          </div>
+          : null
+        }
+          
+            {policies.length ? 
+              <>
+                <div className="font-bold grid grid-cols-8 md:grid-cols-6 gap-x-4 mt-8 border-primary border-t border-b py-2">
+                  <div className="col-span-5 sm:col-span-6 md:col-span-4">Policy</div>
+                  <div className="text-center hidden md:block">Passed</div>
+                  <div className="text-center col-span-3 sm:col-span-2 md:col-span-1">More</div>
+                </div>
+                <ul className="mt-4">
+                  {policies.map((policy, index) => (
+                    <li 
+                      className="search-result border-b border-gray-300 border-dotted last:border-none py-6"
+                      key={`${index}-${policy.policyId}`}
+                      id={`${index}-${policy.policyId}`}
+                    >
+                      <SearchResultItem policy={policy} geographyList={geographyList} texts={policy.resultsByPage} />
+                      {/* <PolicyListItem policy={policy} geographyList={geographyList} /> */}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            :
+              null
+            }
+            
         </div>
     </section>
+    
   )
 }
 export default SearchResults;
