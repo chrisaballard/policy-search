db8906cc010a66388eff84bb295dbcb05a9780ae
Paula Hightower <eurolife@gmail.com>
2021-11-05 15:55:22 +0000

    Add pdf to page

---
+++ app/src/pages/pdf/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react';
+import ViewSDKClient from '../../api/pdf';
+import Head from 'next/head';
+import MainLayout from '../../components/layouts/MainLayout'
+import Script from 'next/script';
+
+const PDFView = () => {
+  const containerRef = useRef();
+  const viewerConfig = {
+    // embedMode: 'IN_LINE',
+    showAnnotationTools: true,
+    enableFormFilling: true,
+  };
+  useEffect(() => {
+    if(containerRef?.current) {
+      const viewSDKClient = new ViewSDKClient();
+      viewSDKClient.ready().then(() => {
+        /* Invoke the file preview and get the Promise object */
+        const previewFilePromise = viewSDKClient.previewFile("pdf-div", viewerConfig);
+      })
+    }
+  }, [containerRef])
+  return (
+    <>
+    <Head>
+      <title>PDF View</title>
+    </Head>
+    <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
+    <MainLayout pageTitle={'PDF View'}>
+      <section className="w-full">
+        <div className="mt-4 max-w-screen-lg mx-auto">
+          <div id="pdf-div" style={{ height: '800px'}} ref={containerRef} >
+
+          </div>
+        </div>
+      </section>
+    </MainLayout>
+    </>
+  )
+}
+
+export default PDFView;
